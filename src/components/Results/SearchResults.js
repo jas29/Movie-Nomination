6a1dca1c0364e2fdf9388cd25e2a2aa647ef5521
Jaskaren <karansaini29@gmail.com>
2020-09-07 13:48:07 -0700

    change request url

--- src/components/Results/SearchResults.js
+++ src/components/Results/SearchResults.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const SearchResults = async (movieName) => {
     try{
         // Commented out to prevent abuse
-        const response = await axios.get(`http://www.omdbapi.com/?s=${movieName}&apikey=2a24e63a&type=movie`)
+        const response = await axios.get(`https://www.omdbapi.com/?s=${movieName}&apikey=2a24e63a&type=movie`)
         // const response = mockData
         return response;
     }
